54ed5bdb6c53335f606e4c32c98dbed8efd067a7
George Charitonidis <gc@icloud.gr>
2024-09-03 15:02:52 +0300

    Removed any kind of authentication

--- App.js
+++ App.js
@@ -1,31 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import BottomBarNav from './app/components/BottomBarNav';
-import LoginScreen from './app/screens/LoginScreen';
-import authenticate from './app/auth/authenticate';
 
 import { enableScreens } from 'react-native-screens';
 
 enableScreens();
 
 export default function App() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-    useEffect(() => {
-        const performAuthentication = async () => {
-            const authenticated = await authenticate();
-            setIsAuthenticated(authenticated);
-        };
-
-        performAuthentication();
-    }, []);
-
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
             <NavigationContainer>
-                {isAuthenticated ? <BottomBarNav /> : <LoginScreen />}
+                <BottomBarNav />
             </NavigationContainer>
         </GestureHandlerRootView>
     );
