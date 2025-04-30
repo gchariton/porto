f6ce018d60ba5f36471a3fcdea46c44ea7e09e91
George Charitonidis <chgeorge@gmail.com>
2025-04-30 10:02:23 +0300

    Checkpoint

--- app/screens/AboutScreen.js
+++ app/screens/AboutScreen.js
@@ -26,7 +26,7 @@ function AboutScreen({}) {
                     />
                 </View>
                 <View style={styles.container}>
-                    <Text style={styles.text}>Updated: 27 Dec 2024</Text>
+                    <Text style={styles.text}>Updated: 30 Apr 2025</Text>
                 </View>
                 <View style={styles.attention}>
                     <Text style={styles.text}>
